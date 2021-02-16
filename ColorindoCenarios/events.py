6105c1e102410291a379a06bfda6e41425058629
Sergio de Almeida Cipriano Junior <sergiosacj@hotmail.com.br>
2021-02-15 22:41:25 -0300

    Create function that treats pygame events
    Co-authored-by: AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>

---
+++ ColorindoCenarios/events.py
@@ -0,0 +1,16 @@
+from pygame import QUIT, MOUSEBUTTONDOWN, mouse
+from colors import WALLS
+from Board import Board
+
+def treats_event(event, board: Board) -> bool:
+    if event.type == QUIT:
+        return False
+    elif event.type == MOUSEBUTTONDOWN:
+        pos = mouse.get_pos()
+        # Change the x/y screen coordinates to grid coordinates
+        column = pos[0] // (board.width + board.margin)
+        row = pos[1] // (board.height + board.margin)
+        # Mark position with new color
+        if not board.out_of_range(row, column):
+            board.grid[row][column] = WALLS
+    return True
