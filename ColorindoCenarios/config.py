b0f7b796d7b2844a1f943b63f0f171322e1fe49b
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-02-19 19:34:25 -0300

    New buttons added
    Co-authored-by: Sergio Cipriano <sergiosacj@hotmail.com.br>

--- ColorindoCenarios/config.py
+++ ColorindoCenarios/config.py
@@ -1,5 +1,5 @@
 import pygame
-from colors import STANDARD_COLOR, RED, GREEN, WALL, BLACK
+from colors import STANDARD_COLOR, RED, GREEN, WALL, BLACK, BLUE, YELLOW, PURPLE, PINK, GREY
 # Classes
 from Mouse import Mouse
 from Button import Button
@@ -18,13 +18,18 @@ pygame.display.set_caption("Colorindo Cenarios")
 clock = pygame.time.Clock()
 
 # All buttons
-draw_button = Button(STANDARD_COLOR, 1095, 5, 460, 205, "DRAW")
-red_button = Button(RED, 1095, 215, 225, 205, "RED")
-green_button = Button(GREEN, 1330, 215, 225, 205, "GREEN")
-buttons = [draw_button, green_button, red_button]
+draw_button = Button(STANDARD_COLOR, 1095, 5, 225, 200, "DRAW")
+grey_button = Button(GREY, 1330, 5, 225, 200, "GREY")
+red_button = Button(RED, 1095, 210, 225, 200, "RED")
+green_button = Button(GREEN, 1330, 210, 225, 200, "GREEN")
+yellow_button = Button(YELLOW, 1095, 410, 225, 200, "YELLOW")
+blue_button = Button(BLUE, 1330, 410, 225, 200, "BLUE")
+purple_button = Button(PURPLE, 1095, 610, 225, 200, "PURPLE")
+pink_button = Button(PINK, 1330, 610, 225, 200, "PINK")
+buttons = [draw_button, grey_button, green_button, red_button, yellow_button, blue_button,purple_button,pink_button]
 
 # Objects
-board = Board(20, 20, 5, STANDARD_COLOR)
+board = Board(25, 25, 5, STANDARD_COLOR)
 mouse = Mouse()
 
 # Controls mouse color
